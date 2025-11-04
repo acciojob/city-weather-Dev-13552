bea53047d39f9e76e37b6eae199ab37bb600c90f
Dev-13552 <devrathour110@gmail.com>
2025-11-04 16:17:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
+  const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+  const [inputValue, setInputValue] = useState("");
+  const [temperature, setTemperature] = useSate("")
+  const [desc, setDesc] = useSate("")
+  const [url, setUrl] = useSate("")
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+   const handleKeyDown = async (event) => {
+    if (event.key === "Enter") {
+      // Prevent the default form submission behavior if inside a form
+      const res = await fetch(` https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_KEY}`)
+      const data = await res.json()
+      setTemperature(data.main.temp)
+      setDesc(data?.weather?.[0]?.description)
+      setUrl(`http://openweathermap.org/img/w/${data?.weather?.[0]?.icon}.png`)
+    }
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <input
+        type="text"
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+      />
 
-export default App
+      <div>{temperature}</div>
+      <div>{weather_description}</div>
+      <img src={url}></img>
+    </div>
+  );
+};
+
+export default App;
