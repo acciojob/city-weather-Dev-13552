95f26cdc1527ac92fd594ccfe90c9e968588c8ae
Dev-13552 <devrathour110@gmail.com>
2025-11-04 16:33:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./../styles/App.css";
+import "regenerator-runtime/runtime";
+
 
 const App = () => {
   const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
